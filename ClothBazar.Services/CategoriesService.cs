887dd40013ba60fb2d12e5840d954f07b93e2712
Bilal Ahmad <bilal19_93@hotmail.com>
2018-12-31 20:56:13 +0500

    CRUD Operations in Entity Framework

---
+++ ClothBazar.Services/CategoriesService.cs
@@ -0,0 +1,55 @@
+﻿using ClothBazar.Database;
+using ClothBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.Services
+{
+    public class CategoriesService
+    {
+        public Category GetCategory(int Id)
+        {
+            using (var context = new CBContext())
+            {
+                return context.Categories.Find(Id);
+            }
+        }
+
+        public List<Category> GetCategories()
+        {
+            using (var context = new CBContext())
+            {
+                return context.Categories.ToList();
+            }
+        }
+        public void SaveCategory(Category category)
+        {
+            using (var context = new CBContext())
+            {
+                context.Categories.Add(category);
+                context.SaveChanges();
+            }
+        }
+        public void UpdateCategory(Category category)
+        {
+            using (var context = new CBContext())
+            {
+                context.Entry(category).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+        public void DeleteCategory(int Id)
+        {
+            using (var context = new CBContext())
+            {
+                //  context.Entry(category).State = System.Data.Entity.EntityState.Deleted;
+                var category = context.Categories.Find(Id);
+                context.Categories.Remove(category);
+                context.SaveChanges();
+            }
+        }
+    }
+}
