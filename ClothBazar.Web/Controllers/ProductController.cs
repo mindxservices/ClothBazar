bca44b518e122f49e320e307a948a5628b8ceff1
Bilal Ahmad <bilal19_93@hotmail.com>
2019-01-10 18:23:26 +0500

    CRUD Operations in One Page using Ajax

---
+++ ClothBazar.Web/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using ClothBazar.Database;
+using ClothBazar.Entities;
+using ClothBazar.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClothBazar.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductsService productServices = new ProductsService();
+        // GET: Product
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+        public ActionResult ProductTable(string search)
+        {
+            var products = productServices.GetProducts();
+            if (!string.IsNullOrEmpty(search))
+            {
+                products = products.Where(p => p.Name.Contains(search)).ToList();
+            }
+            return PartialView(products);
+        }
+        public ActionResult Create()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            productServices.SaveProduct(product);
+            return RedirectToAction("ProductTable");
+        }
+
+    }
+}
